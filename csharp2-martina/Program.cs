e258b7a1c943618e709648a09771a1ac342dab63
Martina Cachová <martinacachova@seznam.cz>
2025-04-10 22:57:57 +0200

    class Car vs static method

--- csharp2-martina/Program.cs
+++ csharp2-martina/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car car = new Car(); //vytvářím objekt typu Car (konstruktor bez parametru)
+        car.SetColor("blue"); //volání nestatické metody na objektu (instanci)
+        Car car2 = new Car("yellow"); //vytvářím další objekt typu Car (konstruktor s parametrem)
+
+        int rectangleArea = Rectangle.CalculateArea(8, 9); //volání statické metody (pracuje pouze se vstupními parametry, které uživatel zadává přímo)
+    }
+
+    class Car
+    {
+        private string color;
+        public Car() { } //defaultní konstruktor není nutný, ale potřebuji ho přidat, pokud mám další konstruktory s paramentrem a chci povolit i bez parametru
+        public Car(string color) //konstruktor s 1 parametrem
+        {
+            this.color = color;
+        }
+        public void SetColor(string color) //protože color je private, potřebuji metodu pro změnu
+        {
+            this.color = color;
+        }
+    }
+
+    class Rectangle
+    {
+        public static int CalculateArea(int a, int b) //static metoda - nevytvářím instanci třídy
+        {
+            return a * b;
+        }
+    }
+}
