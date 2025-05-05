7907b4ef9d69985c5f9d0f22564a7ce85598e116
Martina Cachová <martinacachova@seznam.cz>
2025-05-05 23:58:43 +0200

    L4 ukol planovac udalosti - doplneni LIST a STATS

--- csharp2-martina/L4.Ukol.PlanovacUdalosti/Program.cs
+++ csharp2-martina/L4.Ukol.PlanovacUdalosti/Program.cs
@@ -18,16 +18,22 @@ class Program
     public static string[] NactiEvent(string eventNameDate)
     {
         string[] eventCasti = eventNameDate.Split(';');
+        if (eventCasti.Count() != 3)
+        {
+            throw new Exception("Nelze načíst událost. Slovo EVENT, název a datum události musí být odděleny středníkem.");
+        }
         return eventCasti;
     }
     public static string NactiEventName(string eventNameDate)
     {
         string eventName = NactiEvent(eventNameDate)[1].Trim();
+
         if (string.IsNullOrWhiteSpace(eventName))
         {
             throw new Exception("Nelze zjistit název události.");
         }
         return eventName;
+
     }
     public static DateTime NactiEventDate(string eventNameDate)
     {
@@ -48,12 +54,75 @@ class Program
         //DateTime.TryParseExact(eventDateString, "yyyy-mm-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime eventDate);
 
         return eventDate;
+    }
+    public static void vypisEvents(List<Event> events)
+    {
+        //Vypište všechny uložené akce. Pro každou akci vypište její jméno, datum a počet dní do konání akce (pozor na akce, které již proběhly). 
+        int i = 0;
+        int j = 0;
+        foreach (Event ev in events)
+        {
+            if ((ev.DatumUdalosti - DateTime.Now).Days < 0)
+            {
 
+                if (i == 0)
+                {
+                    Console.WriteLine("Již proběhlé akce: ");
+                    i++;
+                }
+                Console.WriteLine($"Event {ev.JmenoUdalosti} with date {ev.DatumUdalosti} happened before {(ev.DatumUdalosti - DateTime.Now).Days} days");
+            }
+        }
+        foreach (Event ev in events)
+        {
+            if ((ev.DatumUdalosti - DateTime.Now).Days >= 0)
+            {
+                if (j == 0)
+                {
+                    Console.WriteLine("Nastávající akce: ");
+                    j++;
+                }
+                Console.WriteLine($"Event {ev.JmenoUdalosti} with date {ev.DatumUdalosti} will happen in {(ev.DatumUdalosti - DateTime.Now).Days} days");
+            }
+        }
+    }
+    public static void vypisStats(Dictionary<DateTime, int> eventsDictionary, List<Event> events)
+    {
+        //Ke každému datu vypište počet událostí, které se konají v daný den. 
+        int pocetUdalosti;
+
+        foreach (Event ev in events)
+        {
+            if (eventsDictionary.TryGetValue(ev.DatumUdalosti, out pocetUdalosti))
+            {
+                eventsDictionary.Remove(ev.DatumUdalosti);
+                eventsDictionary.Add(ev.DatumUdalosti, pocetUdalosti + 1);
+                pocetUdalosti = 0;
+            }
+            else
+            {
+                eventsDictionary.Add(ev.DatumUdalosti, 1);
+            }
+        }
+        foreach (var keyValuePair in eventsDictionary)
+        {
+            Console.WriteLine($"Date: {keyValuePair.Key}...events: {keyValuePair.Value}");
+        }
     }
 
     static void Main(string[] args)
     {
         List<Event> eventList = new List<Event>();
+        eventList.Add(new Event("lekce1", new DateTime(2025, 5, 8, 18, 0, 0)));
+        eventList.Add(new Event("lekce2", new DateTime(2025, 5, 15, 18, 0, 0)));
+        eventList.Add(new Event("lekce3", new DateTime(2025, 5, 22, 18, 0, 0)));
+        eventList.Add(new Event("hospoda", new DateTime(2025, 5, 22, 18, 0, 0)));
+        eventList.Add(new Event("hospoda", new DateTime(2025, 4, 22)));
+        eventList.Add(new Event("lekce4", new DateTime(2025, 5, 29, 18, 0, 0)));
+
+        //Vytvořte slovník (Dictionary<DateTime, int>), do kterého si uložíte data všech uložených akcí a ke každému datu počet událostí, které se konají v daný den.
+        Dictionary<DateTime, int> eventsDict = new Dictionary<DateTime, int>();
+
         bool chciUkoncitProgram = false;
         do
         {
@@ -67,12 +136,12 @@ class Program
             {
                 case "list":
                     {
-
+                        vypisEvents(eventList);
                         break;
                     }
                 case "stats":
                     {
-
+                        vypisStats(eventsDict, eventList);
                         break;
                     }
                 case "end":
@@ -88,12 +157,12 @@ class Program
                             try
                             {
                                 eventList.Add(new Event(NactiEventName(vstupUzivatele), NactiEventDate(vstupUzivatele)));
-                                Console.WriteLine(eventList[0].DatumUdalosti);
+                                //Console.WriteLine(eventList[0].DatumUdalosti);
                             }
                             catch (Exception exception)
                             {
-                                System.Console.WriteLine(exception.Message);
-                                System.Console.WriteLine("Event nebyl přidán. Zadej znovu.");
+                                Console.WriteLine(exception.Message);
+                                Console.WriteLine("Event nebyl přidán. Zadej znovu.");
                             }
                             break;
                         }
