25fa9f8ba5181bab812dfbd2c9fe77860170a80b
Martina Cachová <martinacachova@seznam.cz>
2025-05-18 22:05:12 +0200

    L6 ukol knihovna ADD

--- csharp2-martina/L6.Ukol.Knihovna/Book.cs
+++ csharp2-martina/L6.Ukol.Knihovna/Book.cs
@@ -1,6 +1,7 @@
 using System;
+using System.Globalization;
 
-namespace L6.Ukol.Book;
+namespace L6.Ukol.Knihovna;
 
 public class Book
 {
@@ -12,8 +13,8 @@ public class Book
     Pages (int) – počet stran
     Použijte veřejné vlastnosti, implementuje vlastní gettery/settery tam, kde se to hodí (např. validace, že počet stran je kladné číslo).*/
 
-    public string Title { get; }
-    public string Author { get; }
+    public string Title { get; private set; }
+    public string Author { get; private set; }
     private DateTime _publishedDate;
     public DateTime PublishedDate
     {
@@ -28,6 +29,7 @@ public class Book
             {
                 _publishedDate = value;
             }
+            //_publishedDate = value > DateTime.Today ? DateTime.Today : value;
         }
     }
     private int _pages;
@@ -44,8 +46,104 @@ public class Book
             {
                 _pages = value;
             }
+            //_pages = value < 0 ? 0 : value;
+        }
+    }
+    // Konstruktor, který inicializuje knihu
+    public Book(string title, string author, DateTime publishedDate, int pages)
+    {
+        Title = title;
+        Author = author;
+        PublishedDate = publishedDate;
+        Pages = pages;
+    }
+
+    // Metody pro zpracování vstupního řetězce (vstup uživatele) ve formátu:
+    // ADD;[název];[autor];[datum vydání ve formátu YYYY-MM-DD];[počet stran]
+    public static string[] SplitStringAdd(string vstupUzivateleAdd)
+    {
+        string[] bookStringCasti = vstupUzivateleAdd.Split(';');
+        if (bookStringCasti.Length != 5)
+        {
+            throw new Exception("Nelze načíst knihu. Slovo ADD, název, autor, datum vydání a počet stran musí být odděleny středníkem.");
+        }
+        return bookStringCasti;
+    }
+    public static string GetTitleFromString(string vstupUzivateleAdd)
+    {
+        string bookTitle = SplitStringAdd(vstupUzivateleAdd)[1].Trim();
+
+        if (string.IsNullOrWhiteSpace(bookTitle))
+        {
+            throw new Exception("Nelze zjistit název knihy.");
+        }
+        return bookTitle;
+    }
+    public static string GetAuthorFromString(string vstupUzivateleAdd)
+    {
+        string bookAuthor = SplitStringAdd(vstupUzivateleAdd)[2].Trim();
+
+        if (string.IsNullOrWhiteSpace(bookAuthor))
+        {
+            throw new Exception("Nelze zjistit autora knihy.");
+        }
+        return bookAuthor;
+    }
+    public static DateTime GetDateFromString(string vstupUzivateleAdd)
+    {
+        /*DateTime bookDate = new DateTime(0001, 01, 01);
+
+        string[] bookDateStrings = SplitStringAdd(vstupUzivateleAdd)[3].Trim().Split('-');
+
+        bool mamRok = int.TryParse(bookDateStrings[0], out int bookDateYear);
+        bool mamMesic = int.TryParse(bookDateStrings[1], out int bookDateMonth);
+        bool mamDen = int.TryParse(bookDateStrings[2], out int bookDateDay);
+
+        if (!mamRok || !mamMesic || !mamDen || bookDateYear == 0 || bookDateMonth == 0 || bookDateDay == 0)
+        {
+            throw new Exception("Nelze zjistit datum vydání.");
         }
 
+        bookDate = new DateTime(bookDateYear, bookDateMonth, bookDateDay);*/
+
+        string datePart = SplitStringAdd(vstupUzivateleAdd)[3].Trim();
+        if (!DateTime.TryParseExact(datePart, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime bookDate))
+        {
+            throw new Exception("Nelze zjistit datum vydání.");
+        }
+        return bookDate;
+    }
+
+    public static int GetPagesCountFromString(string vstupUzivateleAdd)
+    {
+        int bookPagesCount;
+
+        if (!int.TryParse(SplitStringAdd(vstupUzivateleAdd)[4].Trim(), out bookPagesCount))
+        {
+            throw new Exception("Nelze zjistit počet stran knihy.");
+        }
+        return bookPagesCount;
+    }
+    public void ListBooks(List<Book> nazevKnihovny)
+    {
+        //Vypíše všechny knihy, seřazené podle data vydání. Použijte OrderBy
+        System.Console.WriteLine("Vypisuji knihy v knihovně:");
+        //doplnit
+    }
+    public void WriteStats(List<Book> nazevKnihovny)
+    {
+        /*Vypíše:
+            Průměrný počet stran (použijte Select a Average)
+            Počet knih od každého autora (použijte GroupBy)
+            Počet unikatních slov v názvech knih. Použijte SelectMany a rozdělení názvů podle mezer (interpunkci vynechte) pro vytvoření jednoho seznamu všech slov, pak použijte Distinct*/
+        System.Console.WriteLine("Vypisuji statistiky:");
+        //doplnit
+    }
+    public void FindInTitle(List<Book> nazevKnihovny)
+    {
+        //Vyhledá knihy, jejichž název obsahuje dané slovo, bez ohledu na velikost písmen (použijte Where).
+        System.Console.WriteLine("Vypisuji knihy obsahující zadané slovo v jejich názvu:");
+        //doplnit
     }
 
 
--- csharp2-martina/L6.Ukol.Knihovna/Program.cs
+++ csharp2-martina/L6.Ukol.Knihovna/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //ADD;[název];[autor];[datum vydání ve formátu YYYY-MM-DD];[počet stran]
+
+        List<Book> knihovna = new List<Book>();
+        knihovna.Add(new Book("Slabikář", "Petr Spisovný", new DateTime(2025, 5, 8), 55));
+        knihovna.Add(new Book("Animal Farm", "George Orwell", new DateTime(1945, 08, 17), 112));
+
+        bool chciUkoncitProgram = false;
+        do
+        {
+            Console.WriteLine("Zadej text ADD;[název];[autor];[datum vydání ve formátu YYYY-MM-DD];[počet stran], např. ADD;Animal Farm;George Orwell;1945-08-17;112");
+            Console.WriteLine("Nebo zadej text LIST pro výpis knih dle data vydání");
+            Console.WriteLine("Nebo zadej text STATS pro výpis statistik");
+            Console.WriteLine("Nebo zadej text FIND;[klíčové slovo] pro hledání v názvu knih");
+            Console.WriteLine("Nebo zadej text END pro ukončení programu: ");
+            string vstupUzivatele = Console.ReadLine();
+
+            if (vstupUzivatele.Trim().StartsWith("list", StringComparison.OrdinalIgnoreCase))
+            {
+                Book.ListBooks(knihovna);
+            }
+            else if (vstupUzivatele.Trim().StartsWith("stats", StringComparison.OrdinalIgnoreCase))
+            {
+                Book.WriteStats(knihovna);
+            }
+            else if (vstupUzivatele.Trim().StartsWith("find", StringComparison.OrdinalIgnoreCase))
+            {
+                Book.FindInTitle(knihovna);
+            }
+            else if (vstupUzivatele.Trim().StartsWith("end", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Ukončuji program.");
+                chciUkoncitProgram = true;
+                //return;
+            }
+            else if (vstupUzivatele.Trim().StartsWith("add", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    string title = Book.GetTitleFromString(vstupUzivatele);
+                    string author = Book.GetAuthorFromString(vstupUzivatele);
+                    DateTime date = Book.GetDateFromString(vstupUzivatele);
+                    int pages = Book.GetPagesCountFromString(vstupUzivatele);
+
+                    knihovna.Add(new Book(title, author, date, pages));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    Console.WriteLine("Kniha nebyla přidána. Zadej znovu.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Neplatný vstup.");
+            }
+        } while (!chciUkoncitProgram);
     }
 }
