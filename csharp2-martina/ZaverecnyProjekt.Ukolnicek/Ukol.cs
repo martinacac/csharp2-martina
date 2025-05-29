9fed5ce12b084a599b0d44c812d51ceb89644910
Martina Cachová <martinacachova@seznam.cz>
2025-05-29 23:06:07 +0200

    ZP Ukolnicek - Ukol.cs properties

--- csharp2-martina/ZaverecnyProjekt.Ukolnicek/Ukol.cs
+++ csharp2-martina/ZaverecnyProjekt.Ukolnicek/Ukol.cs
@@ -4,5 +4,9 @@ namespace ZaverecnyProjekt.Ukolnicek;
 
 public class Ukol
 {
+    public string Nazev { get; private set; }
+    public int Priorita { get; private set; }
+    public DateTime DatumSplneni { get; private set; }
+    public bool Splneno { get; private set; }
 
 }
